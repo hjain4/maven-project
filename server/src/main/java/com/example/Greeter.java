dd452a06b7511c42c054caa7d355d5e79abe4bdc
hjain4 <jainhimanshu1986@gmail.com>
2017-05-12 18:17:10 +0400

    fix checkstyle warnings

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,7 +12,7 @@ public class Greeter {
 
   }
 /**
-   @param someone the name of person
+   * @param someone the name of person
    */
     public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,5 +1,4 @@
 /**
 * this is my name
 	*/
-
 package com.example;
